ebe103cdd5c2d91d232eeceff2ea13169e3cba61
Victor Garcia <victor.santos@sou.inteli.edu.br>
2025-02-26 16:45:49 -0300

    Modificação da tela de login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -8,15 +8,15 @@ class Login extends Component {
                     <div className='loginMeio'>
                         <h2 className='titulo1'>Conecte sua conta</h2>
                         <div className='container-caixadetexto'>                       
-                            <input className='caixadetexto'></input>
-                            <input className='caixadetexto'></input>
-                            <a className='senhaText'>Esqueceu sua senha?</a>
+                            <input className='caixadetexto' placeholder='Usuário' type='text'/>
+                            <input className='caixadetexto' placeholder='Senha' type='password'/>
+                            <a className='senhaText' href='#'>Esqueceu sua senha?</a>
                         </div>
                         <div className='container-botoes'>
+                            <button className='botao-login'>Entrar</button>
                         </div> 
                     </div>
                 </div>
-                
             </div>
         );
     }

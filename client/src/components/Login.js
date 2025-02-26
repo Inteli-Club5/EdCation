0107a20e6199e8075bb9a65c206f77f34bb18b17
João Pedro Gonçalves <joao.araujo@sou.inteli.edu.br>
2025-02-26 16:24:03 -0300

    Tela de Login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -3,8 +3,20 @@ import React, { Component } from 'react';
 class Login extends Component {
     render() {
         return (
-            <div>
-                <h3>Hello World</h3>
+            <div className='containerLog'>
+                <div className='metade'>
+                    <div className='loginMeio'>
+                        <h2 className='titulo1'>Conecte sua conta</h2>
+                        <div className='container-caixadetexto'>                       
+                            <input className='caixadetexto'></input>
+                            <input className='caixadetexto'></input>
+                            <a className='senhaText'>Esqueceu sua senha?</a>
+                        </div>
+                        <div className='container-botoes'>
+                        </div> 
+                    </div>
+                </div>
+                
             </div>
         );
     }

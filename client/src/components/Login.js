e900c4695ee230689f4ed3572deb9734f0de7054
Victor Garcia <victor.santos@sou.inteli.edu.br>
2025-02-27 09:35:39 -0300

    Debug da Tela de Conta

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -7,27 +7,29 @@ class Login extends Component {
             <div className='containerLog'>
                 <div className='metade'>
                     <div className='loginMeio'>
-                        <div className='content-titulo'>
-                            <h2 className='titulo1'>Conecte sua <strong className='lightgreen'>Conta</strong></h2>
-                        </div>
-                        <div className='container-caixadetexto'>
+                        <div className='ajusteMeio'>
                             <div className='content-titulo'>
-                                <h5 className='textforbox'>Email</h5>
+                                <h2 className='titulo1'>Conecte sua <strong className='lightgreen'>Conta</strong></h2>
                             </div>
-                            <input className='caixadetexto' type='text' />
-                            <div className='content-titulo'>
-                                <h5 className='textforbox'>Senha</h5>
+                            <div className='container-caixadetexto'>
+                                <div className='content-titulo'>
+                                    <h5 className='textforbox'>Email</h5>
+                                </div>
+                                <input className='caixadetexto' type='text' />
+                                <div className='content-titulo'>
+                                    <h5 className='textforbox'>Senha</h5>
+                                </div>
+                                <input className='caixadetexto' type='password' />
                             </div>
-                            <input className='caixadetexto' type='password' />
-                        </div>
-                        <div className='container-botoes'>
-                            <Link to="/criar-conta" >
-                                <button className='botao-login botao-login2'>Criar Conta</button>
-                            </Link>
-                            <Link to="/escolha">
-                                <button className='botao-login'>Entrar</button>
-                            </Link>
-                            <br/>
+                            <div className='container-botoes'>
+                                <Link to="/criar-conta" >
+                                    <button className='botao-login botao-login2'>Criar Conta</button>
+                                </Link>
+                                <Link to="/escolha">
+                                    <button className='botao-login'>Entrar</button>
+                                </Link>
+                                <br />
+                            </div>
                         </div>
                     </div>
                 </div>

f04d33011ffe709cc4a02f5f324906b30fddfd26
João Pedro Gonçalves <joao.araujo@sou.inteli.edu.br>
2025-02-26 22:20:38 -0300

    Tela de Perfil
    fazendo a tela de perfil

--- client/src/components/Conta.js
+++ client/src/components/Conta.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import logo from './images/edcation3.png';
 import profile from './images/profile.png';
 import certificado from './images/certificado.png';
 
 class Conta extends Component {
+    handleLogout = () => {
+        console.log("Usuário saiu da conta");
+    };
+
     render() {
         return (
             <div>
@@ -25,15 +29,30 @@ class Conta extends Component {
                     <hr className="horizontal-line" />
                     <div className="tracks-container">
                         <div className="track-card">
-                            <img src={certificado} className="track-icon-2"></img>
+                            <img src={certificado} className="track-icon-2" alt="Certificado" />
                             <p className="track-name">Certificado #1</p>
                             <p className="access-link access-2">Blockchain</p>
                         </div>
                     </div>
                 </div>
+                <div className="content-container">
+                    <h1 className="title">Perfil</h1>
+                    <hr className="horizontal-line" />
+                    <div className='content-perfil'>
+                        <div className="profile-detail"><strong>Nome:</strong> Mateus Costa </div>
+                        <div className="profile-detail"><strong>Email:</strong> mateus.costa@outlook.com</div>
+                        <div className="profile-detail"><strong>Data de Nascimento:</strong> 08/05/1998 </div>
+                        <div className="profile-detail"><strong>Chave Pública:</strong> 0x6B509c04e3caA2207b8f2A60A067a8ddED03b8d0</div>
+                    </div>
+                    <div className="logout-container">
+                        <Link to='/'>
+                        <button>Sair da Conta</button>
+                        </Link>
+                    </div>
+                </div>
             </div>
-        )
+        );
     }
-};
+}
 
 export default Conta;
